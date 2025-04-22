6c4cb55cc9b0eda84501ea0253d8018f9220c564
MomoMyosotis <molollo73@gmail.com>
2025-04-23 00:05:03 +0200

    15
    implementing some sense in the pet's action calling

--- main.py
+++ main.py
@@ -30,18 +30,29 @@ def get_by_attr(obj_list, target_value, attr_name):
 def main():
 
     initial_check()
-#    cls()
-#    print("\n\n_________________________________________")
+    cls()
+    print("\n\n_________________________________________")
     generator = impslist.random.randint(1,13)
     temp = C.dynamic_pets(generator)
-#    print("_________________________________________\n\n")
+    print("_________________________________________\n\n")
 
 
     while True:
         new_action = impslist.random.randint(1,9)
         related_pet = []
-        related_pet = impslist.random.sample(C.animali, 1)[0]
-        impslist.pet.action(new_action, related_pet)
+        if new_action == 1 or 8:
+            # pesca da lista di non presenti
+            while related_pet not in C.animali:
+                related_pet = impslist.random.sample(C.lar, 1)[0]
+        elif new_action == 2 or 9:
+            #remove from list
+            related_pet = impslist.random.sample(C.lar,1)[0]
+            C.lar.remove(related_pet)
+        elif new_action == 3 or 4 or 5 or 6 or 7:
+            related_pet = impslist.random.sample(C.animali, 1)[0]
+            impslist.pet.action(new_action, related_pet)
+        else:
+            print("error 6")
         impslist.time.sleep(1)
 
 main()
