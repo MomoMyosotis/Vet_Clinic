84e25f83808afb0febc799179deafac36ea6111b
MomoMyosotis <molollo73@gmail.com>
2025-04-29 01:52:17 +0200

    21

---
+++ SQL/credentials.py
@@ -0,0 +1,12 @@
+# first line
+
+# Dettagli della connessione al database
+db_config = {
+    'dbname': 'Owners',
+    'user': 'postgres',
+    'password': '160718',
+    'host': 'localhost',    # Lascia "localhost" se usi PostgreSQL in locale
+    'port': '5432'          # Porta di default per PostgreSQL
+}
+
+# last line
